01935e7e16e7aa853c68837496672ea12817de47
Arya Nandakumar <74709811+Arya-Nandakumar@users.noreply.github.com>
2023-07-11 22:20:25 +0530

    Design Update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
 checkboxes.forEach(checkbox => {
   checkbox.addEventListener('change', updateCount);
 });
+
 function updateCount() {
   const checkedCount = document.querySelectorAll('input[type="checkbox"]:checked').length;
   document.getElementById('count').textContent = ` ${checkedCount}/3`;
@@ -12,3 +15,13 @@ function updateCount() {
     this.parentElement.classList.remove('checked');
   }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  const checklist = document.getElementById('checklist');
+  const firstItemCheckbox = checklist.querySelector('input[type="checkbox"]');
+  const firstItemLabel = firstItemCheckbox.parentElement;
+
+  firstItemCheckbox.checked = true;
+  firstItemLabel.classList.add('checked');
+  updateCount();
+});
